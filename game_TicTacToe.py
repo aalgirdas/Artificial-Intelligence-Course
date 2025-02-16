b897d0db7a3e9351195336562936e48109c20d91
ALGIRDAS LAUKAITIS <algirdas.laukaitis@vgtu.lt>
2025-02-16 10:38:10 +0200

    Add files via upload

---
+++ game_TicTacToe.py
@@ -0,0 +1,30 @@
+
+from games import *
+
+game_object = TicTacToe()
+
+game_object.play_game( random_player, random_player   ) # minmax_player , random_player query_player  alpha_beta_player expect_minmax_player
+
+
+
+
+
+
+
+
+
+
+
+#game_object.play_game( random_player ,  expect_minmax_player ) # minmax_player , random_player query_player  alpha_beta_player expect_minmax_player
+#play_game(    dict(X=random_player, O=player(alphabeta_search))    , verbose=True).utility
+
+#game_object = ConnectFour() #   Gomoku() AL: it takes too long ; naive approuch to check states 3^(6*7) dosn't work ;
+
+
+
+
+
+
+
+
+
